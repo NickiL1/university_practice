97cc6dfe9c12691937b7c30c13e4b09e778c1729
Nick Levin <drlevin@Nicks-MacBook-Pro.local>
2024-04-15 21:49:20 +0300

    small code beautify

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Password obj = new Password();
         System.out.println(obj.isValid("Adfgrerge"));
     }
